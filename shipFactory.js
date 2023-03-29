a438898aedb419b49c8d6bff650e11c58c9f8b69
Nicholas Maahs <105306382+mrwindupbirdesq@users.noreply.github.com>
2023-03-29 20:10:05 +0000

    Write tests for basic ship attributes: length, hits, sunk

--- shipFactory.js
+++ shipFactory.js
@@ -1,11 +1,17 @@
-const shipFactory = (length) => {
+export const shipFactory = (size) => {
   let hits = 0;
+  let sunk = false;
+  const length = size;
   const hit = () => {
     hits += 1;
+  }
+  const checkHits = () => {
     return hits;
   }
+  const isSunk = () => {
+    return sunk;
+  }
   
-return { hit }
+return { hit, checkHits, length, isSunk }
 };
 
-export { shipFactory }
--- shipFactory.test.js
+++ shipFactory.test.js
@@ -8,13 +8,28 @@
 // isSunk() function that calculates true/false based on ship's
 // length and number of hits 
 import * as ship from './shipFactory.js';
+const testBoat = ship.shipFactory(0);
+describe('hit tests', () => {
+  test('does hit() work on new ship', () => {
+    testBoat.hit();
+    expect(testBoat.checkHits()).toBe(1);
+  });
+  test('does hit() increment', () => {
+    testBoat.hit();
+    expect(testBoat.checkHits()).toBe(2);
+  });
+});
 
-    const testBoat = ship.shipFactory('2');
-console.log(testBoat)
-    test('does hit() work on new ship', () => {
-        expect(testBoat.hit()).toBe(1);
-    });
-    test('does hit() increment', () => {
-        expect(testBoat.hit()).toBe(2);
-    });
+describe('create new ship object', () => {
+  const lilboat = ship.shipFactory(2);
+  test('has length', () => {
+    expect(lilboat.length).toBe(2);
+  });
+  test('has 0 hits', () => {
+    expect(lilboat.checkHits()).toBe(0);
+  });
 
+  test('is not sunk', () => {
+    expect(lilboat.isSunk()).toBe(false);
+  });
+})  
