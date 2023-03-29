6804faf974e2d03dd9753925ac74ac76c0919885
Nicholas Maahs <105306382+mrwindupbirdesq@users.noreply.github.com>
2023-03-29 19:38:22 +0000

    Implement passing shipFactory.hit()

--- shipFactory.js
+++ shipFactory.js
@@ -1,6 +1,11 @@
 const shipFactory = (length) => {
-
-return { length }
+  let hits = 0;
+  const hit = () => {
+    hits += 1;
+    return hits;
+  }
+  
+return { hit }
 };
 
 export { shipFactory }
