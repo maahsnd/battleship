6cf408cef1700789711cfc4672acfa4d7412e0e6
Nicholas Maahs <105306382+mrwindupbirdesq@users.noreply.github.com>
2023-03-31 14:28:37 +0000

    Create basic board

--- ui.js
+++ ui.js
@@ -1,8 +1,24 @@
-const createBoard = () => {
-  const createCell = (rowNum) => {
-    return
+const renderBoard = () => {
+  const createCell = () => {
+    let cell = document.createElement('div');
+    cell.classList.add('cell');
+    return cell;
   }
-return { createCell }  
+  const createRow = () => {
+    let row = document.createElement('div');
+    row.classList.add('row');
+    for (let i = 9; i >= 0; i--) {
+      row.appendChild(createCell());
+    }
+    return row;
+  }
+  const createBoard = (hostID) => {
+    let hostDiv = document.getElementById(hostID)
+    let board = document.createElement('div')
+    for (let i = 9; i >= 0; i--) {
+      board.appendChild(createRow());
+    }
+    hostDiv.appendChild(board);
+  }
+return { createBoard }  
 }
-
-export { createBoard }
